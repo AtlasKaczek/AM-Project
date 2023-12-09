5275a218e8f6079d4f5cdb8f625d075ffff08efa
Stanisław Kryj <stan.kryj@gmail.com>
2023-12-09 21:44:34 +0100

    Show Month on start-up

--- src/navigation/Stack.js
+++ src/navigation/Stack.js
@@ -1,5 +1,6 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Login, Register, Home } from "../views";
+import { Month } from '../views/Month';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,6 +11,7 @@ const optionScreen = {
 export default function StackNav() {
     return (
         <Stack.Navigator>
+            <Stack.Screen name="Month" component={Month} options={optionScreen} />
             <Stack.Screen name="Home" component={Home} options={optionScreen} />
             <Stack.Screen name="Login" component={Login} options={optionScreen} />
             <Stack.Screen name="Register" component={Register} options={optionScreen} />
